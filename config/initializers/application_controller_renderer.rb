2ce63f3ea50ab979fa2125260f95e52747cde1e8
AyakaSaito <sayk000303@gmail.com>
2021-06-18 16:31:32 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '68bea7319f9b18f788ee7717c04fe3418505c0b0db462d2d3170324d8cb71d49563838658441cbfe27ef167a660afd076c36580fc67de989dcc05925a9e54e97'
